d78aa530a56efc437ec936d17c77af42470ad0ff
sinan cengiz <sinancengiz_84@hotmail.com>
2021-05-07 11:15:05 -0700

    print statements are removed

--- junior_exercise_3.py
+++ junior_exercise_3.py
@@ -10,14 +10,12 @@ def sinan_junior_exercise_3(dummy_list_of_list):
     # lines below is beening runned if first item in the given list type is a list  if it is not we go else statement, 
     #other wise we cal function with first + what we return from rest of the list code after + sign at line 14 makes we contunius  call the function for rest of the list  
     if type(dummy_list_of_list[0]) == type([]):
-        print(dummy_list_of_list[0])
         return sinan_junior_exercise_3(dummy_list_of_list[0] + sinan_junior_exercise_3(dummy_list_of_list[1:]))
     else:
         # here is where all magic is done, logic here is we return first item in the situation we know it is not a list because otherwise above code will run
         # + plus what we return from the function is will be first item + what we retrun from function until there is no element left 
         # that will return us flattened list 
-        # to better demonsration with given example please look at the excel screen shoot 
-        print(dummy_list_of_list[0])
+        # to better demonsration with given example please look at the excel screenshot image
         return dummy_list_of_list[:1] + sinan_junior_exercise_3(dummy_list_of_list[1:])
 
 
